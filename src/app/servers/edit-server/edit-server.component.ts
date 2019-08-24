17356a04afab5619776f0f1e4bb7af42278f4236
Lori Humphries <lhemailme@aim.com>
2019-08-24 08:35:00 -0400

    adding more routing to code

--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -1,34 +1,59 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { CanComponentDeactivate } from './can-deactivate-guard.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-edit-server',
   templateUrl: './edit-server.component.html',
   styleUrls: ['./edit-server.component.css']
 })
-export class EditServerComponent implements OnInit {
+export class EditServerComponent implements OnInit, CanComponentDeactivate {
   server: {id: number, name: string, status: string};
   serverName = '';
   serverStatus = '';
+  allowEdit = false;
+  changesSaved = false;
 
   constructor(private serversService: ServersService,
-              private route: ActivatedRoute
+              private route: ActivatedRoute,
+              private router: Router
     ) { }
 
   ngOnInit() {
     console.log(this.route.snapshot.queryParams);
     console.log(this.route.snapshot.fragment);
-    this.route.queryParams.subscribe();
-    this.route.fragment.subscribe();
-    this.server = this.serversService.getServer(1);
+    this.route.queryParams.subscribe(
+      (queryParams: Params) => {
+        this.allowEdit = queryParams['allowEdit'] === '1' ? true: false;
+      }
+    );
+    this.route.fragment.subscribe();  
+    const id = +this.route.snapshot.params['id'];
+    this.server = this.serversService.getServer(id);
+    //subscribe route params to update the id if params change
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
   }
 
   onUpdateServer() {
     this.serversService.updateServer(this.server.id, {name: this.serverName, status: this.serverStatus});
+    this.changesSaved = true;
+    this.router.navigate(['../'], {relativeTo: this.route});
+  }
+  canDeactivate(): Observable<boolean> | Promise<boolean> | boolean {
+    //if not allowed to edit, allow user to leave
+    if(!this.allowEdit) {
+      return true;
+    }
+    //if change was made and save changes button not pressed, give message, else allow user to leave
+    if((this.serverName !== this.server.name || this.serverStatus !== this.server.status) && !this.changesSaved) {
+      return confirm("Do you want to discard the changes?");
+    }else {
+      return true;
+    }
   }
 
 }
--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router, Data } from '@angular/router';
 
 @Component({
   selector: 'app-server',
@@ -12,22 +12,36 @@ export class ServerComponent implements OnInit {
   server: {id: number, name: string, status: string};
 
   constructor(private serversService: ServersService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private router: Router
     ) { }
 
   ngOnInit() {
+    //name 'server' below must match the name in the app routing module ServerResolver
+    this.route.data
+       .subscribe(
+         (data: Data) => {
+           this.server = data['server'];
+         }
+       );
      
   //  this.server = this.serversService.getServer(1);
     //we had to add the plus sign below so that the id is treated as a number and not a string 
     //we had to do the same in the observable below (+params)
-      const id = +this.route.snapshot.params['id'];
+
+    //after adding resolver, we comment out this code
+   /*    const id = +this.route.snapshot.params['id'];
       this.server = this.serversService.getServer(id);
        
     this.route.params.subscribe(
       (params: Params) => {
         this.server = this.serversService.getServer(+params['id'])
       } 
-    );   
+    );    */
   }
+  onEdit() {
+    //we added queryParamsHandling with preserve to keep the params as we navigate to the child route
+    this.router.navigate(['edit'], {relativeTo: this.route, queryParamsHandling: 'preserve'});
 
+  }
 }
